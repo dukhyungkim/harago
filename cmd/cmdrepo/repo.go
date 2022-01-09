ec30b1813fe833f59eb467b162f63fbdefff5d20
DukHyung Kim <idealismboy@gmail.com>
2022-01-09 23:12:04 +0900

    example sub command

--- cmd/cmdrepo/repo.go
+++ cmd/cmdrepo/repo.go
@@ -4,12 +4,18 @@ import (
 	"docgo/gservice/gchat"
 	"github.com/dukhyungkim/harbor-client"
 	"google.golang.org/api/chat/v1"
+	"strings"
 )
 
 type CmdRepo struct {
 	name         string
 	harborClient harbor.Client
 }
+
+const (
+	cmdLS   = "ls"
+	cmdList = "list"
+)
 
 func NewRepoCommand(harborClient harbor.Client) *CmdRepo {
 	return &CmdRepo{
@@ -23,6 +29,16 @@ func (c *CmdRepo) GetName() string {
 }
 
 func (c *CmdRepo) Run(event *gchat.ChatEvent) *chat.Message {
+	fields := strings.Fields(event.Message.Text)
+	if fields == nil {
+		return c.Help()
+	}
+
+	switch fields[0] {
+	case cmdLS, cmdList:
+		return &chat.Message{Text: "will show list of repository"}
+	}
+
 	return &chat.Message{Text: "implement me"}
 }
 
