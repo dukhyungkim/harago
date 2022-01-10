3505553e7e0aee14377c71b8f96212da76c80991
DukHyung Kim <idealismboy@gmail.com>
2022-01-11 00:07:13 +0900

    add sample repo list with pagination

--- cmd/cmdrepo/repo.go
+++ cmd/cmdrepo/repo.go
@@ -2,8 +2,11 @@ package cmdrepo
 
 import (
 	"docgo/gservice/gchat"
+	"fmt"
 	"github.com/dukhyungkim/harbor-client"
+	harborModel "github.com/dukhyungkim/harbor-client/model"
 	"google.golang.org/api/chat/v1"
+	"strconv"
 	"strings"
 )
 
@@ -15,6 +18,9 @@ type CmdRepo struct {
 const (
 	cmdLS   = "ls"
 	cmdList = "list"
+
+	minCountList           = 3
+	minCountListPagination = 5
 )
 
 func NewRepoCommand(harborClient harbor.Client) *CmdRepo {
@@ -34,14 +40,78 @@ func (c *CmdRepo) Run(event *gchat.ChatEvent) *chat.Message {
 		return c.Help()
 	}
 
-	switch fields[0] {
+	switch fields[1] {
 	case cmdLS, cmdList:
-		return &chat.Message{Text: "will show list of repository"}
+		if len(fields) < minCountList {
+			return c.Help()
+		}
+		projectName := fields[2]
+		repositoriesParams := harborModel.NewListRepositoriesParams()
+
+		if len(fields) >= minCountListPagination {
+			page, err := strconv.ParseInt(fields[3], 10, 64)
+			if err != nil {
+				return &chat.Message{Text: fmt.Sprintf("Error!: %s", err.Error())}
+			}
+			repositoriesParams.Page = page
+
+			pageSize, err := strconv.ParseInt(fields[4], 10, 64)
+			if err != nil {
+				return &chat.Message{Text: fmt.Sprintf("Error!: %s", err.Error())}
+			}
+			repositoriesParams.PageSize = pageSize
+		}
+
+		repositories, err := c.harborClient.ListRepositories(projectName, repositoriesParams)
+		if err != nil {
+			return &chat.Message{Text: fmt.Sprintf("Error!: %s", err.Error())}
+		}
+		cards := makeRepositoryCard(repositories)
+		return &chat.Message{Text: fmt.Sprintf("list of repository in %s", projectName), Cards: cards}
 	}
 
-	return &chat.Message{Text: "implement me"}
+	return c.Help()
 }
 
 func (c *CmdRepo) Help() *chat.Message {
 	return &chat.Message{Text: "HELP!"}
+}
+
+func makeRepositoryCard(repositories []*harborModel.Repository) []*chat.Card {
+	cards := make([]*chat.Card, len(repositories))
+	for i := range repositories {
+		cards[i] = &chat.Card{
+			Header: &chat.CardHeader{
+				Title: repositories[i].Name,
+			},
+			Sections: []*chat.Section{
+				{
+					Widgets: []*chat.WidgetMarkup{
+						{
+							KeyValue: &chat.KeyValue{
+								TopLabel:         "ArtifactCount",
+								Content:          fmt.Sprint(repositories[i].ArtifactCount),
+								ContentMultiline: true,
+							},
+						},
+						{
+							KeyValue: &chat.KeyValue{
+								TopLabel:         "CreationTime",
+								Content:          repositories[i].CreationTime,
+								ContentMultiline: true,
+							},
+						},
+						{
+							KeyValue: &chat.KeyValue{
+								TopLabel:         "UpdateTime",
+								Content:          repositories[i].UpdateTime,
+								ContentMultiline: true,
+							},
+						},
+					},
+				},
+			},
+		}
+	}
+	return cards
 }
