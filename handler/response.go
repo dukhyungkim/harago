501f07c79caac8f79b2973f03e941b970fe99c18
DukHyung Kim <idealismboy@gmail.com>
2022-02-21 22:06:27 +0900

    add response message

--- handler/response.go
+++ handler/response.go
@@ -1,8 +1,10 @@
 package handler
 
 import (
+	"fmt"
 	pbAct "github.com/dukhyungkim/libharago/gen/go/proto/action"
 	"google.golang.org/api/chat/v1"
+	"harago/common"
 	"harago/gservice/gchat"
 	"harago/repository"
 	"log"
@@ -18,40 +20,56 @@ func NewResponseHandler(gChat *gchat.GChat, repo *repository.DB) *ResponseHandle
 	return &ResponseHandler{gChat: gChat, repo: repo}
 }
 
-func (h *ResponseHandler) NotifyResponse(message *pbAct.ActionResponse) {
-	if message.GetSpace() == "" {
-		h.broadcastMessage(message.GetText())
+func (h *ResponseHandler) NotifyResponse(response *pbAct.ActionResponse) {
+	if response.GetSpace() == "" {
+		h.broadcastMessage(response)
 		return
 	}
-	h.sendMessageToSpace(message.GetSpace(), message.GetText())
+	h.sendMessageToSpace(response.GetSpace(), response)
 }
 
-func (h *ResponseHandler) broadcastMessage(text string) {
+func (h *ResponseHandler) broadcastMessage(response *pbAct.ActionResponse) {
 	spaces, err := h.repo.FindSpaces()
 	if err != nil {
 		log.Println(err)
 		return
 	}
 
-	msg := formatMessage(text)
+	var message *chat.Message
+	switch response.GetType() {
+	case pbAct.ActionType_DEPLOY:
+		message = formatDeployResponse(response)
+	default:
+		log.Println(common.ErrUnknownActionType(response.GetType()))
+		return
+	}
 
 	for _, space := range spaces {
-		go h.gChat.SendMessage(space.Space, msg)
+		go h.gChat.SendMessage(space.Space, message)
 	}
 }
 
-func (h *ResponseHandler) sendMessageToSpace(space, text string) {
-	msg := formatMessage(text)
+func (h *ResponseHandler) sendMessageToSpace(space string, response *pbAct.ActionResponse) {
+	var message *chat.Message
+	switch response.GetType() {
+	case pbAct.ActionType_DEPLOY:
+		message = formatDeployResponse(response)
+	default:
+		log.Println(common.ErrUnknownActionType(response.GetType()))
+		return
+	}
 
-	go h.gChat.SendMessage(space, msg)
+	go h.gChat.SendMessage(space, message)
 }
 
-func formatMessage(message string) *chat.Message {
-	sb := strings.Builder{}
+func formatDeployResponse(response *pbAct.ActionResponse) *chat.Message {
+	deployResp := response.GetRespDeploy()
 
-	sb.WriteString("message from handago\n")
+	sb := strings.Builder{}
+	sb.WriteString(fmt.Sprintf("%s - Done from %s with %s\n",
+		response.GetType().String(), deployResp.GetCompany(), deployResp.GetResourceUrl()))
 	sb.WriteString("```")
-	sb.WriteString(message)
+	sb.WriteString(deployResp.GetText())
 	sb.WriteString("```")
 
 	return &chat.Message{Text: sb.String()}
